011985e431363e86b7d3be8a6f53fbea60c16a17
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-08-08 08:14:44 +0200

    fix(home): Add Logout button to HomeScreen for user session management

--- mobile/app/(main)/home.tsx
+++ mobile/app/(main)/home.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { logout } from "@/services/userService";
 import React from "react";
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button title="Logout" onPress={logout} />
     </View>
   );
 };
