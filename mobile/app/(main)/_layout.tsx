8f7f3507e23a13f97814996060fddec9531a9bc2
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-08-08 08:14:36 +0200

    fix(layout): Implement token check to redirect authenticated users to login page

--- mobile/app/(main)/_layout.tsx
+++ mobile/app/(main)/_layout.tsx
@@ -1,6 +1,11 @@
-import { Stack } from "expo-router";
+import { useAuthStore } from "@/stores/authStore";
+import { Redirect, Stack } from "expo-router";
 
 const MainLayout = () => {
+  const token = useAuthStore((state) => state.token);
+
+  if (token) return <Redirect href="/login" />; // TODO Fix the authStore because when logging in, I get imidietly redirected to the login page again
+
   return <Stack screenOptions={{ headerShown: false }} />;
 };
 
