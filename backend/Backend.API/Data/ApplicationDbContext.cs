9ae061ad305c91126ff6367fc74f490bb4617242
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-08-16 17:51:04 +0200

    refactor: reorder RefreshToken property in ApplicationDbContext and clean up using directives in Household model

--- backend/Backend.API/Data/ApplicationDbContext.cs
+++ backend/Backend.API/Data/ApplicationDbContext.cs
@@ -6,12 +6,13 @@ namespace Backend.API.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<AppUser>(options)
     {
-        public DbSet<RefreshToken> RefreshTokens { get; set; }
         public DbSet<UserHousehold> UserHouseholds { get; set; }
         public DbSet<Household> Households { get; set; }
         public DbSet<ShoppingList> ShoppingLists { get; set; }
         public DbSet<ShoppingItem> ShoppingItems { get; set; }
         public DbSet<StoreTag> StoreTags { get; set; }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
